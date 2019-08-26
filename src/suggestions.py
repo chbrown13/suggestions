f62595130e5a16e4fbb1fa82096c3244cf54edf5
Chris Brown <dcbrown06@gmail.com>
2019-08-26 13:02:45 -0400

    Adding suggestions script for surveys and random sample

--- src/suggestions.py
+++ src/suggestions.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python2.7
 
-# from github import Github, GithubException.RateLimitExceededException
 import csv
 import datetime
 import github
@@ -11,51 +10,8 @@ import sys
 
 SUGGESTION = "```suggestion\r\n"
 CODE = "```"
-
-
-def _get_code(comment):
-    start = comment.index(SUGGESTION) + len(SUGGESTION)
-    try:
-        end = comment.index(CODE, start)
-    except ValueError:
-        end = len(comment)
-    return comment[start:end].replace("\n", "").replace("\r", "").lstrip(" ")
-
-
-def _wget(link):
-    try:
-        src = urllib2.urlopen(link).read()
-    except urllib2.HTTPError:
-        print("ERROR: ", link)
-        return ""
-    return src
-
-
-def is_applied(pull, comment):
-    # Check if the suggested code exists in the latest version
-    code = _get_code(comment.body)
-    if pull.commits > 1:
-        for commit in pull.get_commits():
-            if (
-                comment.path in [f.filename for f in commit.files]
-                and commit.commit.committer.date > comment.created_at
-            ):
-                file_url = (
-                    commit.html_url.replace(
-                        "github.com", "raw.githubusercontent.com"
-                    ).replace("commit/", "")
-                    + "/"
-                    + comment.path
-                )
-                src = _wget(file_url)
-                if any(
-                    [
-                        line.replace("\n", "").lstrip(" ") == code
-                        for line in src.split("\n")
-                    ]
-                ):
-                    return commit
-    return None
+SUGGESTEE = "Hi {user}! We noticed you recently received a comment using the new suggested changes feature on a review for a pull request you submitted to the {repo} repository on GitHub ({link}). Please consider filling out the brief survey below to provide feedback on your experience with this new feature. Your responses will be anonymized and used for research purposes, and completing the survey will give you the chance to win a $100 Amazon gift card! Thanks for your time.\n"
+SUGGESTER = "Hi {user}! We noticed you recently used the new suggested changes feature to review a pull request for the {repo} repository on GitHub ({link}). Please consider filling out the brief survey below to provide feedback on your experience with this new feature. Your responses will be anonymized and used for research purposes, and completing the survey will give you the chance to win a $100 Amazon gift card! Thanks for your time.\n"
 
 def is_suggestion(comment):
     # Check if comment contains code suggestion to user
@@ -63,193 +19,59 @@ def is_suggestion(comment):
         return True
     return False
 
-def check_issues(issue):
-    # Collect data for issues without pull requests
-    time.sleep(5)
-    row = [
-        issue.repository.full_name.encode('utf-8'),
-        issue.number,
-        issue.user.login.encode('utf-8'),
-        issue.closed_by.login.encode('utf-8') if issue.closed_by else "",
-        issue.state.encode('utf-8'),
-        str((issue.closed_at - issue.created_at).total_seconds())
-        if issue.closed_at
-        else "",
-        ";".join([i.name for i in issue.labels]).encode('utf-8'),
-    ]
-    if issue.state == "closed":
-        with open("issAccepted.csv", "a") as f:
-            writer = csv.writer(f, delimiter=",")
-            writer.writerow(row)
-    else:
-        with open("issRejected.csv", "a") as f:
-            writer = csv.writer(f, delimiter=",")
-            writer.writerow(row)
-
-
-def check_pulls(pull):
-    # Collect data from pull requests with no code suggestions
-    time.sleep(5)
-    row = [
-        pull.base.repo.full_name.encode('utf-8'),
-        pull.number,
-        pull.user.login.encode('utf-8'),
-        pull.merged_by.login.encode('utf-8') if pull.merged_by else "",
-        pull.merged,
-        pull.state.encode('utf-8'),
-        str((pull.closed_at - pull.created_at).total_seconds())
-        if pull.closed_at
-        else "",
-        str((pull.merged_at - pull.created_at).total_seconds())
-        if pull.merged_at
-        else "",
-        pull.additions,
-        pull.deletions,
-        pull.changed_files,
-        ";".join([f.filename for f in pull.get_files()]).encode('utf-8'),
-    ]
-    if pull.merged == True:
-        with open("pullsAccepted.csv", "a") as f:
-            writer = csv.writer(f, delimiter=",")
-            writer.writerow(row)
-    else:
-        with open("pullsRejected.csv", "a") as f:
-            writer = csv.writer(f, delimiter=",")
-            writer.writerow(row)
-
+def create_email(pull, comment):
+    email1 = email2 = ''
+    puller = pull.user
+    commenter = comment.user
+    if puller.email is not None:
+        email1 = SUGGESTEE.replace('{user}', puller.login).replace('{repo}', pull.base.repo.name).replace('{link}', pull.html_url)
+        with open('email.txt', 'a') as f:
+            f.write("Suggestee: {email}\n".replace('{email}',puller.email))
+            f.write(email1)
+    if commenter.email is not None:
+        email2 = SUGGESTER.replace('{user}', commenter.login).replace('{repo}', pull.base.repo.name).replace('{link}', pull.html_url)
+        with open('email.txt', 'a') as f:
+            f.write("Suggester: {email}\n".replace('{email}', commenter.email))
+            f.write(email2)
 
 def check_comments(pull):
-    # Parse PR comments to search for code suggestions made by developers
     comments = pull.get_comments()
+    arr = []
     for c in comments:
-        time.sleep(5)
-        row = [
-            pull.base.repo.full_name.encode('utf-8'),
-            pull.number,
-            c.commit_id.encode('utf-8'),
-            pull.user.login.encode('utf-8'),
-            c.user.login.encode('utf-8'),
-            pull.merged_by.login.encode('utf-8') if pull.merged_by else "",
-            pull.merged,
-            pull.state.encode('utf-8'),
-            str((pull.merged_at - pull.created_at).total_seconds())
-            if pull.merged_at
-            else "",
-            "",
-            "",
-            "",
-            c.path.encode('utf-8'),
-            repr(c.body),
-        ]
         if is_suggestion(c.body):
-            applied = is_applied(pull, c)
-            if applied != None:
-                row[9] = str(
-                    (applied.commit.committer.date - c.created_at).total_seconds()
-                )
-                row[10] = str(applied.stats.additions)
-                row[11] = str(applied.stats.deletions)
-                with open("accepted.csv", "a") as f:
-                    writer = csv.writer(f, delimiter=",")
-                    writer.writerow(row)
-                print(row)
-            else:
-                with open("rejected.csv", "a") as f:
-                    writer = csv.writer(f, delimiter=",")
-                    writer.writerow(row)
-                print(row)
-    check_pulls(pull)
+            print c.body.encode('utf-8')
+            arr.append(c)
+    return arr
 
-
-def _setup():
-    # Create .csv files for storing data
-    pull_header = [
-        "repo",
-        "pullID",
-        "dev_user",
-        "review_user",
-        "merged",
-        "state",
-        "resolve_time",
-        "accept_time",
-        "adds",
-        "dels",
-        "changed_files",
-        "files",
-    ]
-    with open("pullsAccepted.csv", "w") as f:
-        writer = csv.writer(f, delimiter=",")
-        writer.writerow(pull_header)
-    with open("pullsRejected.csv", "w") as f:
-        writer = csv.writer(f, delimiter=",")
-        writer.writerow(pull_header)
-    code_header = [
-        "repo",
-        "pullID",
-        "commitID",
-        "dev_user",
-        "sugg_user",
-        "review_user",
-        "merged",
-        "state",
-        "resolve_time",
-        "accept_time",
-        "adds",
-        "dels",
-        "files",
-        "comment",
-    ]
-    with open("accepted.csv", "w") as f:
-        writer = csv.writer(f, delimiter=",")
-        writer.writerow(code_header)
-    with open("rejected.csv", "w") as f:
-        writer = csv.writer(f, delimiter=",")
-        writer.writerow(code_header)
-    issue_header = [
-        "repo",
-        "issueID",
-        "dev_user",
-        "review_user",
-        "state",
-        "resolve_time",
-        "labels",
-    ]
-    with open("issAccepted.csv", "w") as f:
-        writer = csv.writer(f, delimiter=",")
-        writer.writerow(issue_header)
-    with open("issRejected.csv", "w") as f:
-        writer = csv.writer(f, delimiter=",")
-        writer.writerow(issue_header)
-
+def _get_code(comment):
+    if is_suggestion(comment):
+        delim = SUGGESTION
+    else:
+        return None
+    start = comment.index(delim) + len(delim)
+    end = comment.index("```", start)
+    return comment[start:end].replace('\n','').replace('\r','').lstrip(' ')
 
 def main():
-    _setup()
-    git = github.Github("<auth_token>")
-    repos = git.search_repositories("q", sort="forks")
-    d = datetime.datetime(2018, 10, 1)
-    reps = 0
-    prs = 0
-    comments = 0
-    iss = 0
-    for repo in repos:
-        reps += 1
-        for issue in repo.get_issues(state="all"):  # pull requests are issues
-            if d < issue.updated_at:
-                if issue.pull_request == None:  # Just an issue
-                    time.sleep(45)
-                    iss += 1
-                    check_issues(issue)
-                    continue
-                else:  # Pull request
-                    pull = issue.as_pull_request()
-                    time.sleep(45)
-                    comments += pull.get_comments().totalCount
-                    prs += 1
-                    check_comments(pull)
-                print(repo.full_name, issue.number, reps, prs, comments, iss)
-            else:
-                time.sleep(60)
-                break
+    git = github.Github("{auth_token}")
+    issues = git.search_issues("q={S} in:comments".replace('{S}',SUGGESTION), sort="updated")
+    for issue in issues:
+        if issue.pull_request is not None:
+            comments= check_comments(issue.as_pull_request())
+            for comment in comments:
+                create_email(issue.as_pull_request(), comment)
+                with open('email.txt', 'a') as f:
+                    try:
+                        f.write(comment.body.decode('utf-8').strip() + '\n' + issue.html_url + '\n')
+                    except UnicodeEncodeError:
+                        f.write('ERROR: ' + issue.html_url + '\n')
+                        continue
+                # with open('sample.txt', 'a') as f:
+                #     try:
+                #         f.write(issue.html_url + ' ' + comment.html_url + '   ' + _get_code(comment.body) + '\n')
+                #     except UnicodeEncodeError:
+                #         f.write('ERROR: ' + issue.html_url + '\n')
+                #         continue
 
 
 if __name__ == "__main__":
